d08c896755a182734ae573d1c9bac6be258f3ddd
Andrew McSparron <Amcsparron@albanyny.gov>
2024-08-26 08:33:27 -0400

    Set project name in pyproject.toml
    Specify the project's name as "PythonDocxAJM". This change also moves the name to a static configuration, ensuring it is explicitly defined.

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ def get_property(prop, project):
     result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open(project + '/_version.py').read())
     return result.group(1)
 
+
 setup(
     name=project_name,
     version=get_property('__version__', project_name),
